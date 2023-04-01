e4f67b4f250bf4442881bf19f25985fd052b60db
JahmirKS <98054292+JahmirKS@users.noreply.github.com>
2023-04-01 11:43:22 -0500

    mobile page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import backroads from './assets/backroads-app.png'
+import mobile from './assets/Mobile_first.png'
+
+import Site from './components/Site'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const colors = ['bg-teal-500', 'bg-orange-400', 'bg-purple-500']
+  const hover_colors = [
+    ' hover:bg-teal-700',
+    ' hover:bg-orange-600',
+    ' hover:bg-purple-700',
+  ]
+
   return (
-    <div className="flex flex-col items-center w-screen h-screen gap-6 bg-gray-800">
+    <div className="flex flex-col items-center w-screen h-screen gap-6 overflow-scroll bg-gray-800">
       <div className="flex flex-col justify-center w-full font-extrabold text-center bg-teal-500 h-1/4 ">
         <h1 className="text-3xl text-gray-100">
           Welcome to my Frontend Portfolio
@@ -18,33 +29,33 @@ function App() {
           main portfolio reserved for those projects.
         </div>
       </div>
-      <section className="flex flex-col items-center justify-center w-3/4 p-4 text-center bg-purple-600 rounded-3xl">
-        <article className="flex flex-col items-center justify-center gap-4 font-semibold text-gray-100">
-          <header>
-            <h2>
-              <a
-                href="https://candid-centaur-f32f8b.netlify.app/"
-                target="_blank"
-              >
-                Backroads App
-              </a>
-            </h2>
-          </header>
-          <a
-            href="https://candid-centaur-f32f8b.netlify.app/"
-            target="_blank"
-            className=""
-          >
-            <img src={backroads} alt="Backroads App" className="rounded-3xl " />
-          </a>
+      <section className="flex flex-col items-center justify-center w-3/4 gap-4 text-center">
+        <Site
+          title="Backroads App"
+          link="https://candid-centaur-f32f8b.netlify.app/"
+          image={backroads}
+          bgColor={colors[2 % colors.length]}
+          button_color={colors[3 % colors.length]}
+          hover={hover_colors[3 % hover_colors.length]}
+        />
 
-          <a
-            href="https://github.com/TheWonderingGecko/backroads"
-            target="_blank"
-          >
-            Source Code
-          </a>
-        </article>
+        <Site
+          title="Mobile first application"
+          link="https://storied-dolphin-2eae5d.netlify.app/"
+          image={mobile}
+          bgColor={colors[3 % colors.length]}
+          button_color={colors[1 % colors.length]}
+          hover={hover_colors[1 % hover_colors.length]}
+        />
+
+        <Site
+          title="Backroads App"
+          link="https://candid-centaur-f32f8b.netlify.app/"
+          image={backroads}
+          bgColor={colors[1 % colors.length]}
+          button_color={colors[2 % colors.length]}
+          hover={hover_colors[2 % hover_colors.length]}
+        />
       </section>
     </div>
   )
--- src/components/Site.jsx
+++ src/components/Site.jsx
@@ -1,4 +1,43 @@
-const Site = () => {
-  return <div>Site</div>
+import { useState, useEffect } from 'react'
+const Site = (props) => {
+  return (
+    <article
+      className={
+        'flex flex-col items-center justify-center gap-4 p-4 font-semibold  rounded-3xl ' +
+        props.bgColor
+      }
+    >
+      <header>
+        <h2>
+          <a
+            href={props.link}
+            target="_blank"
+            className="text-xl font-extrabold tracking-wider shadow-xl "
+          >
+            {props.title}
+          </a>
+        </h2>
+      </header>
+      <a href={props.link} target="_blank" className="">
+        <img
+          src={props.image}
+          alt={props.title}
+          className="object-cover rounded-3xl "
+        />
+      </a>
+
+      <a
+        href={props.title}
+        target="_blank"
+        className={
+          'p-2 rounded-md font-bold shadow-xl ' +
+          props.button_color +
+          props.hover
+        }
+      >
+        Source Code
+      </a>
+    </article>
+  )
 }
 export default Site
